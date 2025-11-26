221e7b7e89ef9b1a8d9e5bc5738de770f4c46686
Gustavo Morini <gustavommh@gmail.com>
2025-11-26 07:17:58 -0300

    Update archlinux.sh

--- archlinux.sh
+++ archlinux.sh
@@ -7,7 +7,7 @@ xdg-user-dirs-update
 sudo pacman -Sy
 
 # utils
-sudo pacman -S --needed --noconfirm bash-completion ntfs-3g dosfstools exfat-utils htop fastfetch papirus-icon-theme gvfs-smb
+sudo pacman -S --needed --noconfirm bash-completion ntfs-3g dosfstools exfat-utils htop fastfetch papirus-icon-theme pop-icon-theme gvfs-smb vim
 
 # internet
 sudo pacman -S --needed --noconfirm firefox qbittorrent git curl wget
@@ -16,13 +16,13 @@ sudo pacman -S --needed --noconfirm firefox qbittorrent git curl wget
 sudo pacman -S --needed --noconfirm gimp inkscape 
 
 # fonts
-sudo pacman -S --needed --noconfirm ttf-dejavu ttf-liberation noto-fonts ttf-monoid-nerd ttf-victor-mono-nerd ttf-jetbrains-mono-nerd ttf-0xproto-nerd 
+sudo pacman -S --needed --noconfirm ttf-dejavu ttf-liberation noto-fonts
 
 # multimidia
 sudo pacman -S --noconfirm ffmpeg gst-plugins-ugly gst-plugins-good gst-plugins-base gst-plugins-bad gst-libav gstreamer showtime
 
 ## gnome
-sudo pacman -S --needed --noconfirm gdm gnome-shell gnome-control-center gnome-console gnome-calculator gnome-system-monitor gnome-disk-utility gnome-text-editor gnome-tweaks loupe papers gnome-browser-connector gnome-backgrounds
+sudo pacman -S --needed --noconfirm gdm gnome-shell gnome-control-center gnome-console gnome-calculator gnome-system-monitor gnome-disk-utility gnome-text-editor gnome-tweaks loupe papers gnome-browser-connector
 
 #sudo systemctl enable gdm
 
@@ -31,6 +31,8 @@ sudo pacman -S --needed --noconfirm gdm gnome-shell gnome-control-center gnome-c
 #sudo systemctl enable cups
 ### instalar via terminal
 # lpadmin -p Brother -E -v "ipp://192.168.3.133/ipp/print" -m everywhere
+# lpoptions -p Brother -o PageSize=A4
+# lpoptions -d Brother
 
 # games
 # pacman -S --noconfirm retroarch retroarch-assets-ozone libretro-snes9x libretro-mgba libretro-genesis-plus-gx libretro-shaders-slang
