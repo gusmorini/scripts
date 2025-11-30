e72fd99cba4ea668fff141d3ef79c3563d7352df
Gustavo Morini <gustavommh@gmail.com>
2025-11-30 15:51:34 -0300

    Update fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-#remove packs
-sudo dnf rm -y gnome-weather gnome-tour gnome-maps gnome-contacts yelp gnome-characters snapshot gnome-clocks gnome-calendar totem rhythmbox evince
+### REMOVE
+pack=("gnome-weather gnome-tour gnome-maps gnome-contacts yelp gnome-characters snapshot gnome-clocks gnome-calendar")
+cmd="dnf rm -y ${pack[@]}"
+sudo $cmd
 
-# install packs
-sudo dnf in -y git curl btop gimp inkscape fastfetch qbittorrent papers showtime
+### INSTALL
+pack=("git curl gimp inkscape fastfetch qbittorrent zsh")
+cmd="dnf in -y ${pack[@]}"
+sudo $cmd
+
+### CONFIGURE
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
